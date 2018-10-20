899f34a3e69048752e0e1a36acf1b728045bb06c
Hariharan <cvhariharan@protonmail.com>
2018-10-20 16:33:38 +0530

    Track peers

--- index.js
+++ index.js
@@ -1,32 +1,61 @@
-var Node = require('ipfs');
+const Node = require('ipfs');
+const Room = require('ipfs-pubsub-room');
+const publicIP = require('public-ip');
+const externalip = require('externalip');
+
+var peer_ips = {};
 
 var node = new Node({
     EXPERIMENTAL: {
       pubsub: true
+    },
+    config: {
+      Addresses: {
+        Swarm: [
+          '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+        ]
+      }
     }
-});
-
-var ipfsGlobal = node;
+  })
 
 const topic = 'active-stimulus-peers';
 
 var msgReceiver = (msg) => console.log(msg.data.toString());
 
-ipfsGlobal.on('ready', () => {
 
-    ipfsGlobal.pubsub.subscribe(topic, msgReceiver, (err) => {
-        if(err) {
-            throw err;
-        }
-        console.log(`Subscribed to ${topic}`);
-    });
+
+node.on('ready', () => {
+    const room = Room(node, topic);
+
+    node.id(function (err, identity) {
+      if (err) {
+        throw err;
+      }
+      console.log("Identity: "+identity.id);
+    })
+
+    room.on('peer joined', (peer) => {
+        console.log('Peer joined the room', peer)
+    })
     
-    ipfsGlobal.pubsub.peers(topic, (err, peerIds) => {
-        if(err) {
-            throw err;
-        }
-        console.log(peerIds);
-    });
+      room.on('peer left', (peer) => {
+        console.log('Peer left...', peer)
+        console.log("IP left: "+peer_ips[peer.toString()]);
+        delete peer_ips[peer.toString()];
+        console.log("Revised array: "+JSON.stringify(peer_ips));
+      })
+    
+      // now started to listen to room
+      room.on('subscribed', () => {
+        console.log('Now connected!')
+      })
+      
+      room.on('message', (message) => {
+        console.log('From: '+message.from);
+        console.log('Message: '+message.data);
+        peer_ips[message.from] = message.data.toString();
+        console.log("Revised array: "+JSON.stringify(peer_ips));
+      });
 
 });
 
