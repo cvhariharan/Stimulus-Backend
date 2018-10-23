3cd62ba1ed0826c3375958f434957b5ba08cf38e
Hariharan <cvhariharan@protonmail.com>
2018-10-23 18:23:04 +0530

    Periodically send IP

--- index.js
+++ index.js
@@ -1,90 +1,8 @@
-// const Node = require('ipfs');
-// const Room = require('ipfs-pubsub-room');
-// const externalip = require('externalip');
-
-// // var peer_ips = {};
-// var peerID;
-// var node = new Node({
-//     EXPERIMENTAL: {
-//       pubsub: true
-//     },
-//     config: {
-//       Addresses: {
-//         Swarm: [
-//           '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
-//         ]
-//       }
-//     }
-//   });
-
-// const topic = 'active-stimulus-peers';
-
-// var msgReceiver = (msg) => console.log(msg.data.toString());
-
-// var peer_ips = new Map();
-
-// node.on('ready', () => {
-//     const room = Room(node, topic);
-
-//     node.id(function (err, identity) {
-//       if (err) {
-//         throw err;
-//       }
-//       peerID = identity.id;
-//       externalip((err, ip) => {
-//         console.log(ip);
-//         room.broadcast(ip);
-//       });
-      // room.on('peer joined', (peer) => {
-      //   console.log('Peer joined the room', peer);
-      //   room.broadcast(peerID);
-      // })
-    
-      // room.on('peer left', (peer) => {
-      //   console.log('Peer left...', peer)
-      //   console.log("IP left: "+peer_ips.get(peer.toString()));
-      //   // delete peer_ips[peer.toString()];
-      //   peer_ips.delete(peer);
-      //   console.log("Revised array: "+peer_ips);
-      //   writeToFile(peer_ips);
-      // })
-    
-      // Broadcast ip when subscribed to the room
-      // room.on('subscribed', () => {
-      //   console.log('Now connected!')
-        
-      // });
-      
-      // room.on('message', (message) => {
-      //   console.log('From: '+message.from);
-      //   console.log('Message: '+message.data);
-      //   // peer_ips[message.from] = message.data.toString();
-      //   if(message.from != peerID) {
-      //     peer_ips.set(message.from, message.data.toString());
-      //     console.log("Revised array: "+peer_ips);
-      //     writeToFile(peer_ips);
-      //   }
-      // });
-
-//     });
-// });
-
-// function writeToFile(peers) {
-//   var peerIter = peers.entries();
-//   for(const [key, value] of peerIter) {
-//     console.log("File: "+value);
-//   }
-// }
-
 const Node = require('ipfs');
 const Room = require('ipfs-pubsub-room');
-const publicIP = require('public-ip');
 const externalip = require('externalip');
-const cmd = require('node-cmd');
-const fs = require('fs');
 
 // var peer_ips = {};
-const confPath = '/home/hariharan/serv1.conf';
 var peerID;
 var node = new Node({
     EXPERIMENTAL: {
@@ -97,9 +15,10 @@ var node = new Node({
         ]
       }
     }
-  })
+  });
 
 const topic = 'active-stimulus-peers';
+const repeatPeriod = 100000;
 
 var msgReceiver = (msg) => console.log(msg.data.toString());
 
@@ -107,43 +26,46 @@ var peer_ips = new Map();
 
 node.on('ready', () => {
     const room = Room(node, topic);
-    console.log("Ready");
+
     node.id(function (err, identity) {
       if (err) {
         throw err;
       }
       peerID = identity.id;
-      console.log("Identity: "+peerID);
-      // room.on('peer joined', (peer) => {
-      //   console.log('Peer joined the room', peer);
-      //   room.broadcast(peerID);
-      // });
-    
-      room.on('peer left', (peer) => {
-        console.log('Peer left...', peer)
-        console.log("IP left: "+peer_ips.get(peer.toString()));
-        // delete peer_ips[peer.toString()];
-        peer_ips.delete(peer);
-        console.log("Revised array: "+peer_ips);
-        // writeToFile(peer_ips);
+      setInterval(() => {
+        externalip((err, ip) => {
+          console.log(ip);
+          room.broadcast(ip);
+        });
+      }, repeatPeriod);
+      room.on('peer joined', (peer) => {
+        console.log('Peer joined the room', peer);
+        room.broadcast(peerID);
       })
     
-      // now started to listen to room
+      // room.on('peer left', (peer) => {
+      //   console.log('Peer left...', peer)
+      //   console.log("IP left: "+peer_ips.get(peer.toString()));
+      //   // delete peer_ips[peer.toString()];
+      //   peer_ips.delete(peer);
+      //   console.log("Revised array: "+peer_ips);
+      //   writeToFile(peer_ips);
+      // })
+  
       room.on('subscribed', () => {
         console.log('Now connected!')
-        
       });
       
-      room.on('message', (message) => {
-        console.log('From: '+message.from);
-        console.log('Message: '+message.data);
-        // peer_ips[message.from] = message.data.toString();
-        if(message.from != peerID) {
-          peer_ips.set(message.from, message.data.toString());
-          console.log("Revised array: "+peer_ips);
-          // writeToFile(peer_ips);
-        }
-      });
+      // room.on('message', (message) => {
+      //   console.log('From: '+message.from);
+      //   console.log('Message: '+message.data);
+      //   // peer_ips[message.from] = message.data.toString();
+      //   if(message.from != peerID) {
+      //     peer_ips.set(message.from, message.data.toString());
+      //     console.log("Revised array: "+peer_ips);
+      //     writeToFile(peer_ips);
+      //   }
+      // });
 
     });
 });
@@ -152,20 +74,111 @@ node.on('ready', () => {
 //   var peerIter = peers.entries();
 //   for(const [key, value] of peerIter) {
 //     console.log("File: "+value);
-//     var data = 'server '+value;
-//     fs.writeFile(confPath, data, (err) => {
-//       if(err)
-//         throw err;
-//       console.log()
-//     });
 //   }
-//   cmd.get(
-//     'nginx -s reload',
-//     function(err, data, stderr){
-//         console.log('Nginx:\n\n',stderr);
-//     }
-// );
 // }
+
+// const Node = require('ipfs');
+// const Room = require('ipfs-pubsub-room');
+// // const publicIP = require('public-ip');
+// const externalip = require('externalip');
+// // const cmd = require('node-cmd');
+// // const fs = require('fs');
+
+// // var peer_ips = {};
+// // const confPath = '/home/hariharan/serv1.conf';
+// var peerID;
+// var node = new Node({
+//     EXPERIMENTAL: {
+//       pubsub: true
+//     },
+//     config: {
+//       Addresses: {
+//         Swarm: [
+//           '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+//         ]
+//       }
+//     }
+//   })
+
+// const topic = 'active-stimulus-peers';
+// const repeatPeriod = 100000;
+// var msgReceiver = (msg) => console.log(msg.data.toString());
+
+// var peer_ips = new Map();
+
+// node.on('ready', () => {
+//     const room = Room(node, topic);
+//     console.log("Ready");
+//     node.id(function (err, identity) {
+//       if (err) {
+//         throw err;
+//       }
+//       peerID = identity.id;
+//       console.log("Identity: "+peerID);
+      
+//       setInterval(() => {
+//         room.broadcast("Testing...");
+//       });
+//       // externalip((err, ip) => {
+//       //     console.log(ip);
+//       //     room.broadcast("Testing...");
+//       //   });
+//       // room.on('peer joined', (peer) => {
+//       //   console.log('Peer joined the room', peer);
+//       //   room.broadcast(peerID);
+//       // });
+    
+//       room.on('peer left', (peer) => {
+//         console.log('Peer left...', peer)
+//         console.log("IP left: "+peer_ips.get(peer.toString()));
+//         // delete peer_ips[peer.toString()];
+//         peer_ips.delete(peer);
+//         console.log("Revised array: "+peer_ips);
+//         // writeToFile(peer_ips);
+//       })
+    
+//       // now started to listen to room
+//       room.on('subscribed', () => {
+//         console.log('Now connected!')
+        
+//       });
+      
+//       room.on('message', (message) => {
+//         console.log('From: '+message.from);
+//         console.log('Message: '+message.data);
+//         // peer_ips[message.from] = message.data.toString();
+//         if(message.from != peerID) {
+//           peer_ips.set(message.from, message.data.toString());
+//           console.log("Revised array: "+peer_ips);
+//           // writeToFile(peer_ips);
+//         }
+//       });
+
+//     });
+
+//     function broadcast() {
+//       room.broadcast("Testing...");
+//     }
+// });
+
+// // function writeToFile(peers) {
+// //   var peerIter = peers.entries();
+// //   for(const [key, value] of peerIter) {
+// //     console.log("File: "+value);
+// //     var data = 'server '+value;
+// //     fs.writeFile(confPath, data, (err) => {
+// //       if(err)
+// //         throw err;
+// //       console.log()
+// //     });
+// //   }
+// //   cmd.get(
+// //     'nginx -s reload',
+// //     function(err, data, stderr){
+// //         console.log('Nginx:\n\n',stderr);
+// //     }
+// // );
+// // }
 
 
 
