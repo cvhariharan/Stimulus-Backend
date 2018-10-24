36f121bff26fd0ae50d00357afaa6e1af0749988
Hariharan <cvhariharan@protonmail.com>
2018-10-24 16:59:30 +0530

    Sign verification

--- js/server.js
+++ js/server.js
@@ -1,15 +1,23 @@
 const IPFS = require('ipfs-api');
 const express = require('express');
 const fileupload = require('express-fileupload');
+const Web3 = require('web3');
+const ethUtil = require('ethereumjs-util');
 
 var app = express();
 var ipfs = IPFS();
+var web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/08cacc7bdd5144dfaed0ce1893e99021"));
+console.log(web3);
 
 app.use(fileupload());
 app.use(express.urlencoded());
 
 app.post('/upload', (req, res) => {
-    file = req.files.upload;
+    var file = req.files.upload;
+    var authorKey = req.body.pubkey;
+    var signature = req.body.signature;
+    var phrase = req.body.keyphrase;
+
     file.mv('uploads/'+file.name, (err) => {
         if(err) {
             console.log(err);
@@ -20,10 +28,20 @@ app.post('/upload', (req, res) => {
                 console.log(err);
                 return res.send(500, {error: "File could not be added to the network."});
             }
-            res.send(200, { hash: result[0].hash, author: req.body.pubkey});
+            var addr = getAddressFromSig(signature, phrase);
+            console.log(signature+" "+phrase+" Signed by: "+ addr);
+            res.send(200, { hash: result[0].hash, author: addr});
         });
         
     })
 });
+
+function getAddressFromSig(signature, phrase) {
+    const { v, r, s } = ethUtil.fromRpcSig(signature);
+    var signedPubKey = ethUtil.ecrecover(Buffer.from(phrase, 'utf8'), v, r, s);
+    var addrBuf = ethUtil.pubToAddress(signedPubKey);
+    var addr = ethUtil.bufferToHex(addrBuf);
+    return addr;
+}
 
 module.exports = app;
